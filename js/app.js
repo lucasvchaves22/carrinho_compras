de9422c60d8898c4b0924330713bd347623f7994
Lucas Vasconcelos <lucasvchaves22@gmail.com>
2024-03-19 17:55:15 -0300

    Iniciando a criacao das funcoes

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Recuperar a quantidade de produtos, o produto e valor
+// Adicionar o produto ao carrinho, separando o produto do valor
+// Adicionar e somar o subtotal do produto, e o valor total do carrinho
+function adicionar() {
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+}
+
+function limpar() {
+
+}
