a5383816ad02f5a37a42e60534d46573e07b4647
Lucas Vasconcelos <lucasvchaves22@gmail.com>
2024-04-16 15:43:35 -0300

    Adicionando funcao limpar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
-// Recuperar a quantidade de produtos, o produto e valor
+let totalGeral = 0;
+limpar();
+
 // Adicionar o produto ao carrinho, separando o produto do valor
 // Adicionar e somar o subtotal do produto, e o valor total do carrinho
 function adicionar() {
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
+    // Recuperar a quantidade de produtos, o produto e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    // Calcular o preço
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    // Adicionar ao carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+    // Adicionando valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
